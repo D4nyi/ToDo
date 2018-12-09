8cbbab73774641663511347a41b2e39680f55b2b
Dániel Szöllősi <szollosi.daniel0@gmail.com>
2018-12-09 16:16:08 +0100

    Add ToDo Activity Modified

--- app/src/main/java/todo/miskolc/uni/iit/hu/todo/AddToDoActivity.java
+++ app/src/main/java/todo/miskolc/uni/iit/hu/todo/AddToDoActivity.java
@@ -1,15 +1,16 @@
 package todo.miskolc.uni.iit.hu.todo;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import java.util.Calendar;
@@ -18,7 +19,6 @@ import java.util.Date;
 public class AddToDoActivity extends AppCompatActivity {
 
     private static final String[] paths = {"Busy", "Free", "Tentative"};
-    private Spinner spinner;
     private int availability = 1; //0 -> Busy, 1 -> Free, 2 -> Tentative
 
     @Override
@@ -26,7 +26,7 @@ public class AddToDoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
 
-        spinner = findViewById(R.id.spinner);
+        Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, paths);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -62,7 +62,7 @@ public class AddToDoActivity extends AppCompatActivity {
         addEvent(calendar, calendar2, title.getText().toString(), description.getText().toString(), location.getText().toString());
     }
 
-    private void addEvent(Calendar beginTime, Calendar endTime, String title, String description, String location) {
+    private void addEvent(@NonNull Calendar beginTime, @NonNull Calendar endTime, @NonNull String title, @NonNull String description, @NonNull String location) {
         Intent intent = new Intent(Intent.ACTION_INSERT)
                 .setData(CalendarContract.Events.CONTENT_URI)
                 .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
@@ -78,11 +78,10 @@ public class AddToDoActivity extends AppCompatActivity {
     }
 
     public void onChecked(View view) {
-        //code to check if this checkbox is checked!
         CheckBox checkBox = (CheckBox) view;
         if (checkBox.isChecked()) {
-            EditText text = findViewById(R.id.duration);
-            text.setVisibility(View.VISIBLE);
+            RelativeLayout layout = findViewById(R.id.options);
+            layout.setVisibility(View.VISIBLE);
         }
     }
 }
